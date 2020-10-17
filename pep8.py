194065a44814349d5aebe96450cf3bbf06c5263a
urbanskii <marcos.urbanski@gmail.com>
2020-10-17 16:39:56 -0300

    First pep8.py close #1

---
+++ pep8.py
@@ -0,0 +1,23 @@
+"""
+PEP8 - Python Enhancement Proposal
+
+São propostas de melhorias para a linguagem Python
+
+The Zen of Python
+
+import this
+
+A ideia da PEP8 é que possamos escrever códigos Pỳthon de forma Pythônica.
+
+[1] - Utiliza Camel Case para nomes de Classes;
+
+"""
+
+
+class Calculadora:
+    pass
+
+
+class CalculadoraCientifica:
+    pass
+
