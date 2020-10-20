66780725848e5347f021b5a930bda17a8edce735
urbanskii <marcos.urbanski@gmail.com>
2020-10-20 17:25:36 -0300

    close #43 exercicio se04E05.py

--- se04E05.py
+++ se04E05.py
@@ -1,15 +1,14 @@
 """
 
 5 - Leia um número real e imprima a quinta parte desse número.
+
 """
 
 
 def main():
 
     numero_real = 2.5500 ** 2
-
     numero_real = str(numero_real-1)
-
     print(f'Quinta parte do número real: {numero_real[5]}')
 
 
