cfb62a482d9ce6bb03cd6d4993bb55a724e159dd
Marcos Urbanski <marcos.urbanski@gmail.com>
2020-10-22 20:37:09 -0300

    close #65 se04E22.py

--- se04E21.py
+++ se04E21.py
@@ -7,7 +7,6 @@ de conversão é: K = L*0.45, sendo K a massa em quilogramas e L a massa em libr
 
 
 def main():
-
     valor_libras = float(input('Digite o valor em libras: '))
     valor_quilogramas = valor_libras*0.45
     print(f'Valor em libras convertido para quilogramas: {valor_quilogramas}')
