ff9ecefba72a3bc2afc9c30e1c45530a5638662a
urbanskii <marcos.urbanski@gmail.com>
2020-10-28 14:50:31 -0300

    close #74 se04E31.py

---
+++ se04E31.py
@@ -0,0 +1,16 @@
+"""
+
+31 - Leia um número inteiro e imprima o seu antecessor
+
+
+"""
+
+
+def main():
+
+    numero_inteiro = int(input('Informe um numero inteiro: '))
+    print(f'Número antecessor do número informado: {numero_inteiro-1}')
+
+
+if __name__ == '__main__':
+    main()
