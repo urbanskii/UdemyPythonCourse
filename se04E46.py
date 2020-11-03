b655799552358363be31e985b0a064f10ae259ff
Marcos Urbanski <marcos.urbanski@gmail.com>
2020-11-03 16:16:39 -0300

    close #89 se04E46.py

---
+++ se04E46.py
@@ -0,0 +1,18 @@
+"""
+
+46 - Faça um programa que leia um número inteiro positivo de três dígitos (de 100 a 999).
+Gere outro número formado pelos dígitos invertidos do número lido, Exemplo:
+Númerolido = 123
+NúmeroGerado = 321.
+
+"""
+
+
+def main():
+
+    numero = input('Digite o numero de 3 digitos: ')
+    print(f'Numero invertido: {numero[::-1]}')
+
+
+if __name__ == '__main__':
+    main()
