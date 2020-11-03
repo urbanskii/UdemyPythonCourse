f1e456b1f56e7dcd6cede78a3da59287a20116cb
Marcos Urbanski <marcos.urbanski@gmail.com>
2020-11-03 13:55:09 -0300

    42

---
+++ se04E42.py
@@ -0,0 +1,26 @@
+"""
+42 - Receba o salário-base de um funcionário. Calcule e imprima o salário a receber, sabendo-
+se que esse funcionário tem uma gratificação de 5% sobre o salário-base. Além disso,
+ele paga 7% de imposto sobre o salário-base.
+"""
+
+
+def main():
+
+    salario_base = float(input('Digite o sálario base: '))
+    calc_gratificacao = salario_base * 0.05
+    calc_gratificacao = salario_base - calc_gratificacao
+
+    calc_imposto = salario_base * 0.07
+    calc_imposto = salario_base - calc_imposto
+
+    salario_base = salario_base - calc_imposto
+    salario_base = salario_base + calc_gratificacao
+
+    print(f'Salário liquido: {salario_base:.6}')
+    print(f'Gratificação sobre o sálario 5%: {calc_gratificacao}')
+    print(f'Desconto imposto 7%: {calc_imposto}')
+
+
+if __name__ == '__main__':
+    main()
