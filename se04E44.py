b2235f2f496b6163a8936aaf216a2a439c8a776d
urbanskii <marcos.urbanski@gmail.com>
2020-11-05 18:05:56 -0300

    new file se04E44.py

---
+++ se04E44.py
@@ -0,0 +1,23 @@
+"""
+44 - Receba a altura do degrau de uma escada e a altura que o usuário deseja alcançar
+subindo a escada. Calcule e mostre quantos degraus o usuário deverá subir para atingir
+seu objetivo.
+"""
+
+
+def main():
+
+    altura_degrau = float(input('Digite a altura do degrau: '))
+    altura_desejada = float(input('Digite a altura desejada: '))
+    degrau = 0
+    quantidade_degrau = 0
+
+    while altura_desejada != degrau:
+        degrau = degrau + altura_degrau
+        quantidade_degrau += 1
+
+    print(f'Quantidade de degraus necessaria para altura desejada: {quantidade_degrau} ')
+
+
+if __name__ == '__main__':
+    main()
