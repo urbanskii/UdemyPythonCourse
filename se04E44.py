a6f9fdcefb6cd38beab4bd403bf048edd51df939
urbanskii <marcos.urbanski@gmail.com>
2020-11-05 17:54:46 -0300

    new directory

--- se04E44.py
+++
@@ -1,9 +0,0 @@
-"""
-
-44 - Receba a altura do degrau de uma escada e a altura que o usuário deseja alcançar
-subindo a escada. Calcule e mostre quantos degraus o usuário deverá subir para atingir
-seu objetivo.
-
-"""
-
-
