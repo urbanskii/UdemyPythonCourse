55c33836d80ad5add88d591a39423013da8cbadc
Marcos Urbanski <marcos.urbanski@gmail.com>
2020-11-13 18:26:23 -0300

    close #150 se05E06.py

---
+++ secao05/se05E06.py
@@ -0,0 +1,23 @@
+"""
+
+06 - Escreva um programa que, dados dois números inteiros, mostre na tela o maior deles,
+assim como a diferença existente entre ambos.
+
+"""
+
+
+def main():
+
+    valor1 = float(input('Digite um valor1: '))
+    valor2 = float(input('Digite um valor2: '))
+
+    if valor1 > valor2:
+        print(f'Valor1 é maior do que valor2 e a diferença entre eles é :  {valor1 - valor2}')
+    elif valor2 > valor1:
+        print(f'Valor2 é maior do que valor1 e a diferença entre eles é :  {valor2 - valor1}')
+    elif valor2 == valor1:
+        print(f'Os valores são iguais!')
+
+
+if __name__ == '__main__':
+    main()
