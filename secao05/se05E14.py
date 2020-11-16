8ea5be7b31b0cbbcdf19b262990046a64245b417
Marcos Urbanski <marcos.urbanski@gmail.com>
2020-11-16 20:45:32 -0300

    create file se05E14.py

---
+++ secao05/se05E14.py
@@ -0,0 +1,12 @@
+"""
+
+14- A nota final de uma estudante é calculada a partir de três notas atribuidas entre o intervalo
+de 0 até 10, respectivamente, a um trabalho de laboratório, a uma avaliação semestral
+e a um exame final. A média das três notas mencionadas anteriormente obedece aos
+pesos: Trabalho de Laboratório: 2; Avaliação Semestral 3; Exame Final: 5. De acordo
+com o resultado, mostre na tela se o aluno está reprovado (média entre 0 e 2,9), de
+recuperação ( entre 3 e 4,9) ou se foi aprovado. Faça todas as verificações necessárias.
+
+
+"""
+
