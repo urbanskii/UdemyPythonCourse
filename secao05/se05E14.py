609d967e18065bd06f578d7a6e9d2f24ceed98de
Marcos Urbanski <marcos.urbanski@gmail.com>
2020-11-17 16:04:54 -0300

    close #158 se05E14.py

--- secao05/se05E14.py
+++ secao05/se05E14.py
@@ -11,9 +11,20 @@ recuperação ( entre 3 e 4,9) ou se foi aprovado. Faça todas as verificações
 """
 
 def main():
-    nota1 = input('Informe a nota 1')
+    trabalho_laboratorio = float(input('Informe a nota do trabalho de laboratorio: '))
+    avaliacao_semestral = float(input('Informe a nota da avaliação semestral: '))
+    exame_final = float(input('Informe a nota do exame final: '))
 
+    media_final = (trabalho_laboratorio*2 + avaliacao_semestral*3 + exame_final*5)/10
 
+    if 0 <= media_final <= 2.9:
+        print(f'Aluno reprovado! {media_final}')
+    elif 3 <= media_final <= 4.9:
+        print(f'Aluno em recuperação! {media_final}')
+    elif 5 <= media_final <= 10:
+        print(f'Aluno aprovado! {media_final}')
+    else:
+        print(f'Numero incorreto!')
 
 
 if __name__ == '__main__':
