9dd30f19b3a677e257573ffd62d3aff4e78591cc
Marcos Urbanski <marcos.urbanski@gmail.com>
2020-11-18 20:54:31 -0300

    create file se05E20.py

---
+++ secao05/se05E20.py
@@ -0,0 +1,15 @@
+"""
+
+20 - Dados três valores, A, B, C, verificar se eles podem ser valores dos lados de um triângulo
+e, se forem, se é um triângulo escaleno, equilátero ou isóscele, considerando os seguintes conceitos:
+
+    - O comprimento de cada lado de um triângulo é menor do que a soma dos outros dois lados.
+
+    - Chama-se equilátero o triângulo que tem três lados iguais.
+
+    - Denominam-se isósceles o triângulo que tem o comprimento de dois lados iguais.
+
+    - Recebe o nome de escaleno o triângulo que tem os três lados diferentes,
+
+"""
+
