b06aff58ccb6d29efa326e7fde0bf4b7f1b9754f
Marcos Urbanski <marcos.urbanski@gmail.com>
2020-11-20 21:32:41 -0300

    create se05E21.py

---
+++ secao05/se05E22.py
@@ -0,0 +1,30 @@
+"""
+
+22 - Leia a idade e o tempo de serviço de um trabalhador e escreva se ele pode ou não se
+aposentar. As condições para aposentadoria são
+
+    - Ter pelo menos 65 anos,
+
+    - Ou ter trabalhado pelo menos 30 anos,
+
+    - Ou ter pelo menos 60 anos e trabalhado pelo menos 25 anos.
+
+"""
+
+
+def main():
+
+    idade = int(input('Informe a idade: '))
+    tempo_servico = int(input('Tempo de serviço: '))
+
+    try:
+        if idade > 65 or tempo_servico > 30 or tempo_servico > 25:
+            print('Aposentadoria liberada!')
+        else:
+            print('Aposentadoria negada!')
+    except ValueError:
+        print('Valor incorreto!')
+
+
+if __name__ == '__main__':
+    main()
