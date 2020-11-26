69f62da1b1b4aea75001e9de7299db1bb40e3952
urbanskii <marcos.urbanski@gmail.com>
2020-11-26 19:32:45 -0300

    create se05E31.py

---
+++ secao05/se05E31.py
@@ -0,0 +1,14 @@
+"""
+
+31 - Faça um programa que receba a altura e o peso de uma pessoa. De acordo com a tabela
+a seguir, verifique e mostre qual a classificação dessa pessoa.
+
+Altura	                           Peso
+                 Até 60	  Entre 60 e 90 (Inclusive)	  Acima de 90
+Menor que 1,20     A	            D	                   G
+De 1,20 a 1,70	   B	            E	                   H
+Maior que 1,70	   C	            F                      I
+
+
+"""
+
