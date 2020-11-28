909636f9e2ce0023e4dac26c72e8fd60120d081f
urbanskii <marcos.urbanski@gmail.com>
2020-11-27 21:21:12 -0300

    change se05E31.py

--- secao05/se05E31.py
+++ secao05/se05E31.py
@@ -12,3 +12,41 @@ Maior que 1,70	   C	            F                      I
 
 """
 
+
+def main():
+
+    while True:
+        try:
+            altura = float(input('Digite a altura: '))
+            peso = float(input('Digite o peso: '))
+            print('Aperte enter para sair')
+
+            if altura < 1.20 and peso <= 60 or 60 <= peso <= 90 or peso > 90:
+                if peso <= 60:
+                    print(f'Classificação A')
+                elif 60 <= peso <= 90:
+                    print(f'Classificação D')
+                elif peso > 90:
+                    print(f'Classificação G')
+            elif 1.20 < altura < 1.70 and peso <= 60 or 60 <= peso <= 90 or peso > 90:
+                if peso <= 60:
+                    print(f'Classificação B')
+                elif 60 <= peso <= 90:
+                    print(f'Classificação E')
+                elif peso > 90:
+                    print(f'Classificação H')
+            elif altura >= 1.70 and peso <= 60 or 60 <= peso <= 90 or peso > 90:
+                if peso <= 60:
+                    print(f'Classificação C')
+                elif 60 <= peso <= 90:
+                    print(f'Classificação F')
+                elif peso > 90:
+                    print(f'Classificação I')
+
+        except ValueError as e:
+            print(f'Erro: {e}')
+            break
+
+
+if __name__ == '__main__':
+    main()
