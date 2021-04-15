05f490021340d5de39e40865e662d608bf05a7b0
urbanskii <marcos.urbanski@gmail.com>
2021-04-15 14:24:55 -0300

    new file se04E44.py

---
+++ secao05/se05E34.py
@@ -0,0 +1,13 @@
+"""
+
+34 - Leia a nota e o número de faltas de um aluno, e escreva seu conceito. De acordo com a
+tabela abaixo, quando o aluno tem mais de 20 faltas ocorre uma redução de conceito.
+
+NOTA	CONCEITO (ATÉ 20 FALTAS)	CONCEITO (MAIS DE 20 FALTAS)
+9.0 até10.0	A	B
+7.5 até 8.9	B	C
+5.0 até 7.4	C	D
+4.0 até 4.9	D	E
+0.0 até 3.9	E	E
+
+"""
